c3ad82d8ff7a9c5328dc1bdb187bc433678f879c
prannoy47 <36932968+prannoy47@users.noreply.github.com>
2020-04-12 14:13:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,18 @@
-//include http, fs and url module
-var http = require('http'),
-    fs = require('fs'),
-    url = require('url');
- 
- 
-//create http server listening on port 3333
-http.createServer(function (req, res) {
-    //use the url to parse the requested url and get the image name
-    var query = url.parse(req.url,true).query;
-        pic = query.image;
- 
-    //read the image using fs and send the image content back in the response
-    fs.readFile('https://github.com/prannoy47/demo-jenkins-build/blob/master/HPE-Container-Platform_logo_1.png' + pic, function (err, content) {
-        if (err) {
-            res.writeHead(400, {'Content-type':'text/html'})
-            console.log(err);
-            res.end("No such image");    
-        } else {
-            //specify the content type in the response will be an image
-            res.writeHead(200,{'Content-type':'image/jpg'});
-            res.end(content);
-        }
-    });
+var http = require('http'),                                                
+    Stream = require('stream').Transform,                                  
+    fs = require('fs');                                                    
 
-// listen on localhost:8000
+var url = 'https://github.com/prannoy47/demo-jenkins-build/blob/master/HPE-Container-Platform_logo_1.png';                    
+
+http.request(url, function(response) {                                        
+  var data = new Stream();                                                    
+
+  response.on('data', function(chunk) {                                       
+    data.push(chunk);                                                         
+  });                                                                         
+
+  response.on('end', function() {                                             
+    fs.writeFileSync('image.png', data.read());                               
+  });                                                                         
 }).listen(8000);
 console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
