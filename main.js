290b8c537b29bf0e9e970f76193ba53a46fbadac
prannoy47 <36932968+prannoy47@users.noreply.github.com>
2020-04-12 14:25:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end(<h1 style="color:green; font-size:5em; font-weight:100;">
-                        Hello Containers...
-                    </h1>);
+  response.end("Hello Containers!! \n");
 });
 
 // listen on localhost:8000
